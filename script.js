9e9a6ddb4b50a43e7b99534851c2573e72c11a89
dhaneshdhkarey <dhaneshdhakrey@gmail.com>
2024-08-11 02:38:34 +0530

    Aug 10
    till ui desing
    
    time 2: 28

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
-console.log("Hello God this is my first script of my first projec");
+console.log("Hello God this is my first script of my first project");
 
-async function getsongs(){
-let a=await fetch("http://127.0.0.1:3000/songs")
-let response=await a.text();
-// console.log(response);
-let divhtml=document.createElement("div")
-divhtml.innerHTML=response;
-let as=divhtml.getElementsByTagName("a")
-let songs=[];
-for(let i=0;i<as.length;i++){
-    const Element1=as[i]
-    if(Element1.href.endsWith("mp3")){
-        songs.push(Element1.href);
+let currentsong=new Audio();
+async function getsongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs");
+    let response = await a.text();
+    let divhtml = document.createElement("div");
+    divhtml.innerHTML = response;
+    let as = divhtml.getElementsByTagName("a");
+    let songs = [];
+    for (let i = 0; i < as.length; i++) {
+        const Element1 = as[i];
+        if (Element1.href.endsWith("mp3")) {
+            songs.push(Element1.href.split("/songs/")[1]);
+        }
     }
+    console.log(songs);
+    return songs;
 }
-console.log(songs);
-return songs
+function playsong(track) {
+    // let audio = new Audio("/songs/"+track);
+    currentsong.src="/songs/"+track;
+    currentsong.play();
 }
-async function main(){
-    let songs= await getsongs();
-    var audio = new Audio(songs[0]);
- audio.play();
+async function main() { 
+    currentsong;
+    let songs = await getsongs();
+    let songsUL = document.querySelector(".songslist").getElementsByTagName("ul")[0];
+
+    for (const song of songs) {
+        songsUL.innerHTML = songsUL.innerHTML + `   
+        <li>
+                        <img class="invert" src="music.svg" alt="">
+                        <div class="songinfo">
+                            <div >${song.replaceAll("%20", " ")}</div>
+                            <div>By Dhanesh</div>
+                            
+                            
+                        </div><!-- song info closes here -->
+                        <div class="playnow">
+                           <span> Play Now </span>
+                        <img class="invert" src="play.svg" alt="">
+                    </div></li>`;
+    }
+    Array.from(document.querySelector(".songslist").getElementsByTagName("li")) .forEach(e=>{
+        e.addEventListener("click",element=>{
+            console.log(e.querySelector(".songinfo").firstElementChild.innerHTML);
+            playsong(e.querySelector(".songinfo").firstElementChild.innerHTML)
+        })
+        
+    });
+
+    return songs;
 }
 
 
 
-document.getElementById("playbutton").addEventListener("click",main);
+main().then(songs => {
+    document.getElementById("playbutton").addEventListener("click", () => playsong(songs));
+});
